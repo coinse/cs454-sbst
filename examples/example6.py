69d3b2c617b50e511b40b1857dba4cc807fc583e
ntrolls <shin.yoo@kaist.ac.kr>
2023-10-19 13:07:54 +0900

    updates example6.py

--- examples/example6.py
+++ examples/example6.py
@@ -1,18 +1,12 @@
-import sys
-
 def match(x: int)->None:
 	y = x * 2 - 3
 	match y:
 		case 5:
 			print("hit 5!")
-		case 10:
+		case 11:
 			print("hit 10!")
-		case -57 | 42:
+		case -57 | 43:
 			print("really?")
 		case _:
 			print("you haven't done anything yet")
 	return
-
-if __name__ == '__main__':
-	x = int(sys.argv[1])
-	match(x)
