b16aa448e69e3617e2f7ec632d179f831d785a61
ntrolls <shin.yoo@kaist.ac.kr>
2024-08-06 14:46:46 +0900

    adding a new example

---
+++ examples/example7.py
@@ -0,0 +1,9 @@
+def sum_of_even(x: int) -> int:
+    if x <= 0:
+        return 0
+    else:
+        s = 0
+        for i in range(x):
+            if not (i % 2 == 1):
+                s += if
+        return s
